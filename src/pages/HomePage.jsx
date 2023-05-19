9f3d9826b2f83b3016e9afc6e839162666e5267b
Roshan <roshan.jha@hih7.in>
2023-05-19 16:39:00 +0530

    Categories Frontend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Layout/Header";
 import Hero from "../components/Route/Hero/Hero";
+import Categories from "../components/Route/Categories/Categories";
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1} />
       <Hero />
+      <Categories />
     </div>
   );
 };
