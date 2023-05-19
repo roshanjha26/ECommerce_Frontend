f921857da1f15c41ccfc78ee2e3f737e555bafba
Roshan <roshan.jha@hih7.in>
2023-05-19 16:05:13 +0530

    Hero Frontend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "../components/Layout/Header";
+import Hero from "../components/Route/Hero/Hero";
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1} />
+      <Hero />
     </div>
   );
 };
