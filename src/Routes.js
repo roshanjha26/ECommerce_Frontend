8aa84cea670786876f3364a3222f82639dabfb5b
Roshan <roshan.jha@hih7.in>
2023-05-19 15:38:03 +0530

    Header Frontend with search and filter functionalities

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import ActivationPage from "./pages/ActivationPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
-export { LoginPage, SignupPage, ActivationPage };
+export { LoginPage, SignupPage, ActivationPage, HomePage };
