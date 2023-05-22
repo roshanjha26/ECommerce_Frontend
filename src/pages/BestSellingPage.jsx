b6bdf451d5156e6542bcadce671b3b70c6a7a12a
Roshan <roshan.jha@hih7.in>
2023-05-22 14:43:24 +0530

    BestSelling page Add in Frontend

--- src/pages/BestSellingPage.jsx
+++ src/pages/BestSellingPage.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Header from "../components/Layout/Header";
+import styles from "../styles/styles";
+import { useSearchParams } from "react-router-dom";
+import { productData } from "../static/data";
+import ProductCards from "../components/Route/ProductCards/ProductCards";
 
 const BestSellingPage = () => {
-  return <div>BestSellingPage</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const d =
+      productData && productData.sort((a, b) => b.total_sell - a.total_sell);
+    setData(d);
+  }, []);
+  return (
+    <div>
+      <Header activeHeading={2} />
+      <br />
+      <br />
+      <div className={`${styles.section}`}>
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
+          {data &&
+            data.map((i, index) => <ProductCards data={i} key={index} />)}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BestSellingPage;
